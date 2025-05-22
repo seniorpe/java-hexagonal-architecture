1863bca7d09668dd7a1e06a2673348fa3911daf5
Fortunato Fernández <dev@senior.pe>
2025-05-22 00:43:30 -0500

    Add Kibana

--- src/main/java/com/bcp/security/infrastructure/web/controller/HealthController.java
+++ src/main/java/com/bcp/security/infrastructure/web/controller/HealthController.java
@@ -1,6 +1,9 @@
 package com.bcp.security.infrastructure.web.controller;
 
 import com.bcp.security.infrastructure.web.dto.response.ApiResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +17,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/health")
 public class HealthController {
+    private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
@@ -21,6 +25,13 @@ public class HealthController {
         Map<String, String> status = new HashMap<>();
         status.put("status", "UP");
         status.put("service", "Security Microservice");
+
+         logger.info("Este es un mensaje de log de nivel INFO");
+        logger.warn("Este es un mensaje de log de nivel WARN");
+        logger.error("Este es un mensaje de log de nivel ERROR");
+        logger.debug("Este es un mensaje de log de nivel DEBUG");
+        logger.trace("Este es un mensaje de log de nivel TRACE");
+
         return Mono.just(ApiResponse.success(status));
     }
 }
